e18f403cf98ff6282d4bfa7ac5aeb467263f9546
Adrian Prajsnar <adrian.prajsnar@tuatara.pl>
2025-01-28 13:15:38 +0100

    Add new route - getTourStats (aggregation)

--- src/routes/tours.ts
+++ src/routes/tours.ts
@@ -5,11 +5,13 @@ import {
     deleteTour,
     getAllTours,
     getTour,
+    getTourStats,
     updateTour,
 } from '../controllers/tourController'
 
 const toursRouter: Router = express.Router()
 
+toursRouter.route('/tour-stats').get(getTourStats)
 toursRouter.route('/top-5-cheap').get(aliasTopTours, getAllTours)
 toursRouter.route('/').get(getAllTours).post(createTour)
 toursRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
