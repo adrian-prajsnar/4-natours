e3f1cf2222b909ba9692d66cfbb241515495eb4c
Adrian Prajsnar <adrian.prajsnar@tuatara.pl>
2025-02-11 12:26:40 +0100

    Handle unhandled routes

--- src/app.ts
+++ src/app.ts
@@ -16,10 +16,6 @@ export const app: Express = express()
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
-app.use((_req: Request, _res: Response, next: NextFunction) => {
-    console.log('Hello from the middleware... 👋')
-    next()
-})
 app.use((req: Request, _res: Response, next: NextFunction) => {
     req.requestTime = new Date().toISOString()
     next()
@@ -28,3 +24,10 @@ app.use((req: Request, _res: Response, next: NextFunction) => {
 // 2) MOUNTING ROUTES
 app.use('/api/v1/tours', toursRouter)
 app.use('/api/v1/users', usersRouter)
+
+app.all('*', (req: Request, res: Response) => {
+    res.status(404).json({
+        statuss: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`,
+    })
+})
