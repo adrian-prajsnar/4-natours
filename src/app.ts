23f3dba755ac6731cccafe2458ee0fb918387e5b
Adrian Prajsnar <adrian.prajsnar@tuatara.pl>
2025-02-12 08:49:32 +0100

    Add better error handling and refactor

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express, { Express, NextFunction, Request, Response } from 'express'
 import morgan from 'morgan'
+import AppError from './utils/appError'
 import toursRouter from './routes/tours'
 import usersRouter from './routes/users'
+import globalErrorHandler from './controllers/errorController'
 
 declare module 'express-serve-static-core' {
     interface Request {
@@ -25,32 +27,8 @@ app.use((req: Request, _res: Response, next: NextFunction) => {
 app.use('/api/v1/tours', toursRouter)
 app.use('/api/v1/users', usersRouter)
 
-interface CustomError extends Error {
-    status?: string
-    statusCode?: number
-    message: string
-}
-
 app.all('*', (req: Request, _res: Response, next: NextFunction) => {
-    const err: CustomError = new Error(
-        `Can't find ${req.originalUrl} on this server!`
-    )
-    err.status = 'fail'
-    err.statusCode = 404
-
-    next(err)
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
 })
 
-app.use(
-    (err: CustomError, _req: Request, res: Response, next: NextFunction) => {
-        err.statusCode = err.statusCode ?? 500
-        err.status = err.status ?? 'error'
-
-        res.status(err.statusCode).json({
-            status: err.status,
-            message: err.message,
-        })
-
-        next()
-    }
-)
+app.use(globalErrorHandler)
