2e36c65d8d1fbd458f8d4af795622ea7da9e0dc5
Adrian Prajsnar <adrian.prajsnar@tuatara.pl>
2025-02-21 09:02:30 +0100

    Send different errors on prod and dev

--- src/controllers/errorController.ts
+++ src/controllers/errorController.ts
@@ -1,6 +1,49 @@
 import { NextFunction, Request, Response } from 'express'
 import AppError from '../utils/appError'
 
+interface SendErrorOptions {
+  err: AppError
+  res: Response
+  status: string
+  statusCode: number
+}
+
+const sendErrorDev = ({
+  err,
+  res,
+  status,
+  statusCode,
+}: SendErrorOptions): void => {
+  res.status(statusCode).json({
+    status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  })
+}
+
+const sendErrorProd = ({
+  err,
+  res,
+  status,
+  statusCode,
+}: SendErrorOptions): void => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational)
+    res.status(statusCode).json({
+      status,
+      message: err.message,
+    })
+  // Programming or other unknown error: don't leak error details
+  else {
+    console.error('ERROR ❌', err)
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    })
+  }
+}
+
 function globalErrorHandler(
   err: AppError,
   req: Request,
@@ -10,10 +53,10 @@ function globalErrorHandler(
   const statusCode: number = err.statusCode || 500
   const status: string = err.status || 'error'
 
-  res.status(statusCode).json({
-    status,
-    message: err.message,
-  })
+  const errorOptions: SendErrorOptions = { err, res, status, statusCode }
+
+  if (process.env.NODE_ENV === 'development') sendErrorDev(errorOptions)
+  else if (process.env.NODE_ENV === 'production') sendErrorProd(errorOptions)
 
   next()
 }
