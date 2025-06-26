5c463e4ba3099c91c70256865a05ee801021daf7
Adrian Prajsnar <adrian.prajsnar11@gmail.com>
2025-06-26 16:29:29 +0200

    Add missing resources to import and delete dev data

--- dev-data/data/import-dev-data.ts
+++ dev-data/data/import-dev-data.ts
@@ -2,6 +2,8 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import { Tour } from '../../src/models/tourModel'
+import { User } from '../../src/models/userModel'
+import { Review } from '../../src/models/reviewModel'
 
 dotenv.config({
   path: ['.env.local', '.env'],
@@ -27,10 +29,14 @@ async function connectToDb(): Promise<void> {
 void connectToDb()
 
 const tours: string = fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
+const users: string = fs.readFileSync(`${__dirname}/users.json`, 'utf-8')
+const reviews: string = fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
 
 async function importData(): Promise<void> {
   try {
     await Tour.create(JSON.parse(tours))
+    await User.create(JSON.parse(users), { validateBeforeSave: false })
+    await Review.create(JSON.parse(reviews))
     console.log('Data successfully imported!')
   } catch (error) {
     console.error('Error importing data', error)
@@ -42,6 +48,8 @@ async function importData(): Promise<void> {
 async function deleteData(): Promise<void> {
   try {
     await Tour.deleteMany()
+    await User.deleteMany()
+    await Review.deleteMany()
     console.log('Data successfully deleted!')
   } catch (error) {
     console.error('Error importing data', error)
