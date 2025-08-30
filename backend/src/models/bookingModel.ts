546f1bb276d69133bdbb3f88a62319c67536a30c
Adrian Prajsnar <adrian.prajsnar11@gmail.com>
2025-08-30 10:20:16 +0200

    Add booking model

---
+++ backend/src/models/bookingModel.ts
@@ -0,0 +1,47 @@
+import mongoose, { model, Query } from 'mongoose'
+import { ITour } from './tourModel'
+import { IUser } from './userModel'
+
+interface IBooking {
+  _id: string
+  tour: ITour
+  user: IUser
+  price: number
+  createdAt: Date
+  paid: boolean
+}
+
+const bookingSchema = new mongoose.Schema({
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Booking must belong to a Tour!'],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Booking must belong to a User!'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Booking must have a price.'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+})
+
+bookingSchema.pre(/^find/, function (this: Query<IBooking, IBooking>, next) {
+  this.populate('user').populate({
+    path: 'tour',
+    select: 'name',
+  })
+  next()
+})
+
+export const Bookings = model<IBooking>('Booking', bookingSchema)
